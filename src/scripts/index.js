610419b23832cd6ca1aaa6effe0a428dbd2b636c
juan-soares <juan.css013@gmail.com>
2024-08-02 08:48:26 -0300

    feat: add forward & backward track functionality.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,29 @@
 const tracklist = [
   {
     title: "Front-End",
-    original:"Dance of the Knights - Prokofiev",
+    original: "Dance of the Knights - Prokofiev",
     track: "./public/assets/tracks/frontend.mp3",
-    feat: "HTML, CSS & JavaScript."
+    feat: "HTML, CSS & JavaScript.",
   },
   {
     title: "Front-End Remix",
-    original:"Dance of the Knights - Prokofiev (Remix)",
+    original: "Dance of the Knights - Prokofiev (Remix)",
     track: "./public/assets/tracks/frontend-remix.mp3",
-    feat: "Angular, React & Vue."
+    feat: "Angular, React & Vue.",
   },
   {
     title: "Back-End",
-    original:"Requiem - Mozart",
+    original: "Requiem - Mozart",
     track: "./public/assets/tracks/backend.mp3",
-    feat: "Java, Node.js & Phyton."
+    feat: "Java, Node.js & Phyton.",
   },
   {
     title: "Back-End Remix",
-    original:"Requiem - Mozart (Remix)",
+    original: "Requiem - Mozart (Remix)",
     track: "./public/assets/tracks/backend-remix.mp3",
-    feat: "Spring Boot, Express & Django."
+    feat: "Spring Boot, Express & Django.",
   },
-]
-
+];
 
 const h1TrackTitle = document.getElementById("track-title");
 const h2TrackOriginal = document.getElementById("track-original");
@@ -36,29 +35,45 @@ const btnForward = document.getElementById("btn-forward");
 
 let actualTrack = 0;
 
-function toggleReproduction() {
-  if (audioPlayer.paused) {
-    btnPlayPause.children[0].classList.remove("fa-play");
-    btnPlayPause.children[0].classList.add("fa-pause");
-    audioPlayer.play();
-  } else {
-    btnPlayPause.children[0].classList.remove("fa-pause");
-    btnPlayPause.children[0].classList.add("fa-play");
-    audioPlayer.pause();
-  }
-}
-
-function forwardTrack(){
-  actualTrack++;
-  if(actualTrack === tracklist.length - 1) {
-    btnForward.disabled = true;
-  }
-  
+function updateTrackInfo() {
   h1TrackTitle.textContent = tracklist[actualTrack].title;
   h2TrackOriginal.textContent = tracklist[actualTrack].original;
   spanFeat.textContent = tracklist[actualTrack].feat;
   audioPlayer.src = tracklist[actualTrack].track;
 }
 
+function playTrack() {
+  btnPlayPause.children[0].classList.remove("fa-play");
+  btnPlayPause.children[0].classList.add("fa-pause");
+  audioPlayer.play();
+}
+
+function pauseTrack() {
+  btnPlayPause.children[0].classList.remove("fa-pause");
+  btnPlayPause.children[0].classList.add("fa-play");
+  audioPlayer.pause();
+}
+
+function toggleReproduction() {
+  if (audioPlayer.paused) {
+    playTrack();
+  } else {
+    pauseTrack();
+  }
+}
+
+function forwardTrack() {
+  actualTrack++;
+  updateTrackInfo();
+  playTrack();
+}
+
+function backwardTrack() {
+  actualTrack--;
+  updateTrackInfo();
+  playTrack();
+}
+
 btnPlayPause.addEventListener("click", toggleReproduction);
-btnForward.addEventListener("click", forwardTrack)
+btnForward.addEventListener("click", forwardTrack);
+btnBackward.addEventListener("click", backwardTrack);
