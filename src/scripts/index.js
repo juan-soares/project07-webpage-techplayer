e5f7581a558fb6e6475f3b212efe4a4b3804e542
juan-soares <juan.css013@gmail.com>
2024-08-02 08:23:22 -0300

    feat: add song files.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,40 @@
+const tracklist = [
+  {
+    title: "Front-End",
+    original:"Dance of the Knights - Prokofiev",
+    track: "./public/assets/tracks/frontend.mp3",
+    feat: "HTML, CSS & JavaScript."
+  },
+  {
+    title: "Front-End Remix",
+    original:"Dance of the Knights - Prokofiev (Remix)",
+    track: "./public/assets/tracks/frontend-remix.mp3",
+    feat: "Angular, React & Vue."
+  },
+  {
+    title: "Back-End",
+    original:"Requiem - Mozart",
+    track: "./public/assets/tracks/backend.mp3",
+    feat: "Java, Node.js & Phyton."
+  },
+  {
+    title: "Back-End Remix",
+    original:"Requiem - Mozart (Remix)",
+    track: "./public/assets/tracks/backend-remix.mp3",
+    feat: "Spring Boot, Express & Django."
+  },
+]
+
+
+const h1TrackTitle = document.getElementById("track-title");
+const h2TrackOriginal = document.getElementById("track-original");
+const spanFeat = document.getElementById("track-feat");
 const audioPlayer = document.querySelector("audio");
 const btnBackward = document.getElementById("btn-backward");
 const btnPlayPause = document.getElementById("btn-play-pause");
 const btnForward = document.getElementById("btn-forward");
+
+let actualTrack = 0;
 
 function toggleReproduction() {
   if (audioPlayer.paused) {
@@ -15,4 +48,17 @@ function toggleReproduction() {
   }
 }
 
+function forwardTrack(){
+  actualTrack++;
+  if(actualTrack === tracklist.length - 1) {
+    btnForward.disabled = true;
+  }
+  
+  h1TrackTitle.textContent = tracklist[actualTrack].title;
+  h2TrackOriginal.textContent = tracklist[actualTrack].original;
+  spanFeat.textContent = tracklist[actualTrack].feat;
+  audioPlayer.src = tracklist[actualTrack].track;
+}
+
 btnPlayPause.addEventListener("click", toggleReproduction);
+btnForward.addEventListener("click", forwardTrack)
